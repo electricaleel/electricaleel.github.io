a78023c94a830623d8223c0ff6bef55b10dcbd53
electricaleel <octopusware@gmail.com>
2020-09-16 08:24:15 +0900

    nullチェック修正

--- JavaScript/First_steps/What_is_JavaScript/scripts/player.js
+++ JavaScript/First_steps/What_is_JavaScript/scripts/player.js
@@ -1,12 +1,12 @@
+const DEF_NAME = 'プレイヤー 1: ';
 const para = document.querySelector('p');
 para.addEventListener('click', updateName);
 
 function updateName() {
-  let name = prompt('名前を入力して下さい');
+  let name = DEF_NAME;
+  name = prompt('名前を入力して下さい');
   
   if (name !== null) {
-    para.textContent = 'プレイヤー 1: ' + name;
-  } else {
-    para.textContent = '名無しさん';
+    para.textContent = DEF_NAME + name;
   }
 }
